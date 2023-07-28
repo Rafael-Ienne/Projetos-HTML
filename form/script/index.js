dd6bfe922c464a82c6d5744bf1951c4b573db0c5
Rafael Ienne <rafaelienne4@gmail.com>
2023-07-28 14:37:36 -0300

    form with animations

--- form/script/index.js
+++ form/script/index.js
@@ -1,12 +1,20 @@
-const skillItems = document.querySelectorAll('.skill_item');
+// Select all elements with the class 'skill_item' and store them in the skillItems variable as a NodeList.
+const skillitems=document.querySelectorAll('.skill_item');
 
-skillItems.forEach(item => {
-    const checkbox = item.querySelector('input[type="checkbox"]');
-    checkbox.addEventListener('change', () => {
-        if (checkbox.checked) {
-            item.style.backgroundColor = '#1fa51f7c'; // Change item color to green when checked
+// Loop through each skill item element using the forEach method.
+skillitems.forEach(item=> {
+    // For each item, find the checkbox element within it and store it in the checkbox variable.
+    const checkbox=item.querySelector('input[type="checkbox"]');
+     // Add a 'change' event listener to the checkbox.
+    checkbox.addEventListener('change',() => {
+        // When the checkbox state changes (checked or unchecked), this function will be executed.
+        if(checkbox.checked) {
+            // If the checkbox is checked, change the background color of the skill item to green (#00FF00).
+            item.style.backgroundColor='#00FF00';
         } else {
-            item.style.backgroundColor = '#FFFFFF'; // Change item color to white when not checked
+            // If the checkbox is not checked, change the background color of the skill item to white (#FFFFFF).
+            item.style.backgroundColor='#FFFFFF';
         }
-    });
-});
+    })
+
+})
